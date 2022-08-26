3dc9709f940ea87eac52ff7c44df2f75babbfab0
wanjiku <annritawanjiku77@gmail.com>
2022-08-26 07:57:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const maintag = document.getElementById("main")
+maintag.remove();
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id","victory")
+// document.body.append(newHeader)
+// h1.id="victory"
+
+newHeader.innerHTML =`<h1>YOUR-NAME is the champion<h1>`
+
